75885b00f27768e431d7163ea09d8214a413a24b
USER_NAMEyjacobo3573 <ysandovalja@ualr.edu>
2021-12-10 20:13:52 +0000

    Dragon and Cycle

---
+++ cycle/assets/js/local-calls.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+$('img[alt="testing image"]').cycle({ 
+		fx: 'scrollUp', // choose your transition type, ex: fade, scrollUp, shuffle, etc... 
+	 sync:   0,
+    delay: -2000 
+   
+	});
+	
+}); 
---
+++ dragon/assets/js/JS local-calls.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+$('.img-center').cycle({ 
+	fx: 'scrollUp', // choose your transition type, ex: fade, scrollUp, shuffle, etc... 
+	 sync:   0,
+    delay: -2000 
+   
+	});
+	
+}); 
