6410173a8a139fc7d81ed98b78cdbdb55ce5c89f
USER_NAMEyjacobo3573 <ysandovalja@ualr.edu>
2021-12-12 17:44:05 +0000

    Updates

--- cycle/assets/js/local-calls.js
+++ cycle/assets/js/local-calls.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-$('img[alt="testing image"]').cycle({ 
+$('.gallery').cycle({ 
 		fx: 'scrollUp', // choose your transition type, ex: fade, scrollUp, shuffle, etc... 
 	 sync:   0,
     delay: -2000 
---
+++ dragon-revised- final/assets/js/JS local-calls.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+$('.ss').cycle({ 
+		fx: 'shuffle', // choose your transition type, ex: fade, scrollUp, shuffle, etc... 
+	 sync:   0,
+    delay: -2000 
+   
+	});
+	
+}); 
